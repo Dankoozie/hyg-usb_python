4e209b038b891c84f5afc48ca2e35cc94ce09732
Dan <dave@kilcorney.com>
2017-01-12 16:12:05 +0000

    Add files via upload

--- hyg-usb.py
+++ hyg-usb.py
@@ -8,13 +8,6 @@ LED_ON = 65
 LED_OFF = 66
 LED_FLASH = 67
 
-def unp(packed):
-    tem = (packed[0] << 8) + packed[1]
-    hige = (packed[2] << 8) + packed[3]
-    hyg = round(125.0 * tem / 65536.0 - 6.0,2) ;
-    temp = round(175.72 * hige / 65536.0 - 46.85,2) ;
-    return(temp,hyg)
-
 class Hyg():
     def __init__(self,dev):
         self.dev = dev
@@ -28,24 +21,30 @@ class Hyg():
         self.dev.write(1,do)
         
     def read_raw(self):
+        #Gets raw sensor values byte array
         self.wr()
         a = self.dev.read(0x81,21)
         return a
 
     def read_dec(self):
-            return unp(self.read_raw())
+        #Gets sensor values as decimal tuple
+        packed = self.read_raw()
+        h = round(125* ((packed[0] << 8) + packed[1]) / 65536.0 - 6.0,2)
+        t = round(175.72 * ((packed[2] << 8) + packed[3]) / 65536.0 - 46.85,2)
+        return(t,h)
+        
     def read_string(self):
+        #Gets sensor values as a text string
         r = self.read_dec()
         return "Temp: " + str(r[0]) + " degrees C" + "\nHyg: " + str(r[1]) +"%"
 
-#Find USB device
+#Find USB device - probably doesn't work for more than 1 device
 def get_dev():
     dev = usb.core.find(idVendor=0x4d8,idProduct=0xf2c4)
 
     if dev is None:
         raise ValueError('Cannot find device. This is a terrible shame')
     else:
-        #print("Device found!")
         #Try to use it if found
         dev.set_configuration()
 
@@ -53,9 +52,6 @@ def get_dev():
 
 if __name__ == "__main__":
     a = Hyg(get_dev())
-    a.red = LED_ON
-    a.orange = LED_ON
-    a.green = LED_FLASH
     while(1):
         print(a.read_string())
         sleep(10)
